39555864af16bb61b25452c1e82adfd4b3abaae3
siraj <shaiksiraj175@gmail.com>
2023-02-11 12:45:52 +0000

    loginApp

---
+++ src/components/Home/index.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import './index.css'
+
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <Message isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
